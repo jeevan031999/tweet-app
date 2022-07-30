d0520a3bdf475d32aea8f4f234680810d7ac6978
nikhil <nikhilwandhare021@gmail.com>
2022-07-30 23:36:59 +0530

    added entity

---
+++ src/main/java/com/cts/tweetapp/exception/Exception_UserDoesNotExists.java
@@ -0,0 +1,8 @@
+package com.cts.tweetapp.exception;
+
+public class Exception_UserDoesNotExists extends Exception{
+    public Exception_UserDoesNotExists(String msg){
+        super(msg);
+    }
+
+}
---
+++ src/main/java/com/cts/tweetapp/exception/InvalidUsernameException.java
@@ -0,0 +1,7 @@
+package com.cts.tweetapp.exception;
+
+public class InvalidUsernameException extends Exception{
+    public InvalidUsernameException(String msg){
+        super(msg);
+    }
+}
